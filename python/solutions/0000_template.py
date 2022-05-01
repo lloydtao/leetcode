ae154e99f8769700efaef4b6fad780542f2024a5
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-05-01 14:48:19 +0100

    Create solution template

---
+++ python/solutions/0000_template.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""
+[Problem description.]
+"""
+from typing import Dict, List, Optional, Set, Tuple
+
+
+class Solution:
+    def solve(self, input: int) -> int:
+        """[Solution description.]"""
+        return True
+
+
+class Test:
+    def test(self):
+        """[Test description.]"""
+        case = [0]
+        expected = 1
+
+        assert Solution().solve(*case) == expected
