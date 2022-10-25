f6bffaa6be6edbedb242b995494631dc63b5d45a
Lewis Lloyd <41923457+LloydTao@users.noreply.github.com>
2022-10-26 00:17:17 +0100

    Update comments on 1. Two Sum

--- python/solutions/0001_two_sum.py
+++ python/solutions/0001_two_sum.py
@@ -8,17 +8,47 @@ from typing import Dict, List, Optional, Set, Tuple
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        """For each number, check if a previous number is its complement.
-        If not, add it to the list of known complements."""
-        # Initialise store of integers
+        """Take each number and check if its complement has been seen before. If not,
+        add it to the list of known complements along with its index.
+
+        Args:
+            nums (List[int]): Input array of integers
+            target (int): Target integer
+
+        Returns:
+            List[int]: Indices of the two integers that sum to target
+        """
+        # Initialise hash map to store known integers
         complements = {}
+        # Iterate through the list
         for i in range(len(nums)):
-            # Check if a previous number is its complement
+            # Check if the current number's complement has been seen before
             complement = target - nums[i]
             if complement in complements:
                 return [complements[complement], i]
-            # Add the current number as a known complement
+            # Add the current number to the list of known complements
             complements[nums[i]] = i
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        """Take each pair of numbers and see if they add up to the target.
+
+        Args:
+            nums (List[int]): Input array of integers
+            target (int): Target integer
+
+        Returns:
+            List[int]: Indices of the two integers that sum to target
+        """
+        # Get length of input array
+        n = len(nums)
+        # Iterate over all pairs (i,j)
+        for i in range(n):
+            for j in range(i + 1, n):
+                # Check if this pair equals the target
+                if nums[i] + nums[j] == target:
+                    return [i, j]
 
 
 class Test:
